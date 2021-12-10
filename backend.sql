c619d73a37c387da68c51f3a5f6118cb468710c0
Serena Zeng <srnazng@gmail.com>
2021-12-10 14:59:29 -0500

    Add files via upload

--- backend.sql
+++ backend.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE IF NOT EXISTS Travel_Reservation_System;
-USE Travel_Reservation_System;
+CREATE DATABASE IF NOT EXISTS Travel_Reservation_System2;
+USE Travel_Reservation_System2;
 
 CREATE TABLE IF NOT EXISTS `Account` (
 	`account_num` int unsigned NOT NULL,
@@ -12,17 +12,17 @@ CREATE TABLE IF NOT EXISTS `Account` (
 CREATE TABLE IF NOT EXISTS `Admin` (
    `account_num` int unsigned NOT NULL,
    PRIMARY KEY (`account_num`),
-   FOREIGN KEY (`account_num`) REFERENCES `Account`(`account_num`) ON DELETE CASCADE
+   FOREIGN KEY (`account_num`) REFERENCES `Account`(`account_num`) ON DELETE CASCADE on update cascade
 );
 CREATE TABLE IF NOT EXISTS `Customer_rep` (
    `account_num` int unsigned NOT NULL,
    PRIMARY KEY (`account_num`),
-   FOREIGN KEY (`account_num`) REFERENCES `Account`(`account_num`) ON DELETE CASCADE
+   FOREIGN KEY (`account_num`) REFERENCES `Account`(`account_num`) ON DELETE CASCADE on update cascade
 );
 CREATE TABLE IF NOT EXISTS `Customer` (
    `account_num` int unsigned NOT NULL,
    PRIMARY KEY (`account_num`),
-   FOREIGN KEY (`account_num`) REFERENCES `Account`(`account_num`)
+   FOREIGN KEY (`account_num`) REFERENCES `Account`(`account_num`) ON DELETE CASCADE on update cascade
 );
 
 INSERT INTO ACCOUNT(`account_num`, `first_name`, `last_name`, `username`, `password`) VALUES (1, 'AdminFirst', 'AdminLast', 'Admin', 'Admin');
@@ -39,13 +39,14 @@ CREATE TABLE IF NOT EXISTS `Airline` (
 
 insert into Airline (`id`) values ('AA');
 insert into Airline (`id`) values ('UA');
+insert into Airline (`id`) values ('SA');
 
 CREATE TABLE IF NOT EXISTS `Owns_aircraft` (
   `aircraft_number` int unsigned NOT NULL,
   `id` char(2) NOT NULL,
   `total_seats` int unsigned DEFAULT 0,
   PRIMARY KEY (`aircraft_number`),
-  FOREIGN KEY (`id`) REFERENCES `Airline` (`id`) 
+  FOREIGN KEY (`id`) REFERENCES `Airline` (`id`) on delete cascade on update cascade
 );
 
 insert into Owns_aircraft (aircraft_number, id, total_seats) values (1, 'AA', 100);
@@ -54,6 +55,10 @@ insert into Owns_aircraft (aircraft_number, id, total_seats) values (3, 'AA', 15
 insert into Owns_aircraft (aircraft_number, id, total_seats) values (4, 'UA', 6);
 insert into Owns_aircraft (aircraft_number, id, total_seats) values (5, 'UA', 200);
 insert into Owns_aircraft (aircraft_number, id, total_seats) values (6, 'UA', 150);
+insert into Owns_aircraft (aircraft_number, id, total_seats) values (7, 'SA', 40);
+insert into Owns_aircraft (aircraft_number, id, total_seats) values (8, 'SA', 50);
+insert into Owns_aircraft (aircraft_number, id, total_seats) values (9, 'SA', 90);
+
 
 CREATE TABLE IF NOT EXISTS `Airport` (
 	`id` char(3) NOT NULL,
@@ -68,8 +73,8 @@ CREATE TABLE IF NOT EXISTS `Operates_at` (
 	`airline_id` char(2) NOT NULL,
     `airport_id` char(3) NOT NULL,
     PRIMARY KEY (`airline_id`, `airport_id`),
-    FOREIGN KEY (`airline_id`) REFERENCES `Airline` (`id`),
-    FOREIGN KEY (`airport_id`) REFERENCES `Airport` (`id`)
+    FOREIGN KEY (`airline_id`) REFERENCES `Airline` (`id`) on delete cascade on update cascade,
+    FOREIGN KEY (`airport_id`) REFERENCES `Airport` (`id`) on delete cascade on update cascade
 );
 
 insert into Operates_at (airline_id, airport_id) values ('AA', 'EWR');
@@ -91,10 +96,10 @@ CREATE TABLE IF NOT EXISTS `Flight` (
     `economy_fare` float,
     `first_class_fare` float,
     PRIMARY KEY (`airline_id`, `flight_number`),
-    FOREIGN KEY (`airline_id`) REFERENCES `Airline` (`id`),
-    FOREIGN KEY (`aircraft_number`) REFERENCES `Owns_aircraft` (`aircraft_number`),
-    FOREIGN KEY (`depart_airport_id`) REFERENCES `Airport` (`id`),
-    FOREIGN KEY (`destination_airport_id`) REFERENCES `Airport` (`id`)
+    FOREIGN KEY (`airline_id`) REFERENCES `Airline` (`id`) on delete cascade on update cascade,
+    FOREIGN KEY (`aircraft_number`) REFERENCES `Owns_aircraft` (`aircraft_number`) on delete cascade on update cascade,
+    FOREIGN KEY (`depart_airport_id`) REFERENCES `Airport` (`id`) on delete cascade on update cascade,
+    FOREIGN KEY (`destination_airport_id`) REFERENCES `Airport` (`id`) on delete cascade on update cascade
 );
 
 insert into Flight (airline_id, flight_number, aircraft_number, 
@@ -143,26 +148,12 @@ CREATE TABLE IF NOT EXISTS `Operation_days` (
     `flight_number` int unsigned NOT NULL,
 	`day` varchar(20),
     PRIMARY KEY (`airline_id`, `flight_number`, `day`),
-    FOREIGN KEY (`airline_id`, `flight_number`) REFERENCES `Flight` (`airline_id`, `flight_number`)
+    FOREIGN KEY (`airline_id`, `flight_number`) REFERENCES `Flight` (`airline_id`, `flight_number`) on delete cascade on update cascade
 );
 insert into Operation_days (airline_id, flight_number, day) values ('AA', 1, 'Monday');
 insert into Operation_days (airline_id, flight_number, day) values ('AA', 1, 'Wednesday');
 insert into Operation_days (airline_id, flight_number, day) values ('AA', 1, 'Thursday');
 insert into Operation_days (airline_id, flight_number, day) values ('UA', 3, 'Wednesday');
-
-CREATE TABLE IF NOT EXISTS `Domestic` (
-	`airline_id` char(2) NOT NULL,
-    `flight_number` int unsigned NOT NULL,
-    PRIMARY KEY (`airline_id`, `flight_number`),
-    FOREIGN KEY (`airline_id`, `flight_number`) REFERENCES `Flight` (`airline_id`, `flight_number`)
-);
-CREATE TABLE IF NOT EXISTS `International` (
-	`airline_id` char(2) NOT NULL,
-    `flight_number` int unsigned NOT NULL,
-    PRIMARY KEY (`airline_id`, `flight_number`),
-    FOREIGN KEY (`airline_id`, `flight_number`) REFERENCES `Flight` (`airline_id`, `flight_number`)
-);
-
 
 CREATE TABLE IF NOT EXISTS `Reserve_ticket` (
 	`ticket_number` int unsigned NOT NULL,
@@ -171,8 +162,10 @@ CREATE TABLE IF NOT EXISTS `Reserve_ticket` (
     `purchase_time` datetime,
     `total_fare` float,
     `booking_fee` float,
+    `economy_cancellation_fee` float default 0.00,
+    `cancelled` bool default false,
     PRIMARY KEY (`ticket_number`),
-	FOREIGN KEY (`account_num`) REFERENCES `Customer` (`account_num`)
+	FOREIGN KEY (`account_num`) REFERENCES `Customer` (`account_num`) on delete cascade on update cascade
 );
 insert into Reserve_ticket (ticket_number, account_num, class, purchase_time, total_fare, booking_fee) values (1, 3, 'business', '2021-11-30 22:51:00', 100.00, 10.00);
 insert into Reserve_ticket (ticket_number, account_num, class, purchase_time, total_fare, booking_fee) values (5, 3, 'business', '2021-11-30 22:51:00', 100.00, 10.00);
@@ -181,8 +174,8 @@ CREATE TABLE IF NOT EXISTS `Portfolio` (
 	`ticket_number` int unsigned NOT NULL,
     `account_num` int unsigned,
     PRIMARY KEY (`ticket_number`),
-    FOREIGN KEY (`ticket_number`) REFERENCES `Reserve_ticket` (`ticket_number`) on delete cascade,
-    FOREIGN KEY (`account_num`) REFERENCES `Customer` (`account_num`)
+    FOREIGN KEY (`ticket_number`) REFERENCES `Reserve_ticket` (`ticket_number`) on delete cascade on update cascade,
+    FOREIGN KEY (`account_num`) REFERENCES `Customer` (`account_num`) on delete cascade on update cascade
 ); 
 insert into Portfolio (ticket_number, account_num) values (1, 3);
 
@@ -194,15 +187,14 @@ CREATE TABLE IF NOT EXISTS `Flight_instance` (
     `economy_seats_available` int unsigned DEFAULT 0,
     `first_class_seats_available` int unsigned DEFAULT 0,
     PRIMARY KEY (`airline_id`, `flight_number`, `departure_date`),
-    FOREIGN KEY (`airline_id`, `flight_number`) REFERENCES `Flight` (`airline_id`, `flight_number`)
+    FOREIGN KEY (`airline_id`, `flight_number`) REFERENCES `Flight` (`airline_id`, `flight_number`) on delete cascade on update cascade
 );
 
 insert into Flight_instance (airline_id, flight_number, departure_date, business_seats_available, economy_seats_available, first_class_seats_available) 
 	values ('AA', 1, '2021-12-1', 1, 2, 3);
 insert into Flight_instance (airline_id, flight_number, departure_date, business_seats_available, economy_seats_available, first_class_seats_available)
 	values ('UA', 3, '2021-12-1', 3, 4, 5);
-insert into Flight_instance (airline_id, flight_number, departure_date, business_seats_available, economy_seats_available, first_class_seats_available)
-	values ('UA', 2, '2021-12-3', 3, 4, 0);
+insert into Flight_instance (airline_id, flight_number, departure_date, business_seats_available, economy_seats_available, first_class_seats_available) values ('UA', 2, '2021-12-3', 3, 4, 0);
     
 CREATE TABLE IF NOT EXISTS `Waiting_list` (
 	`airline_id` char(2) NOT NULL,
@@ -211,11 +203,11 @@ CREATE TABLE IF NOT EXISTS `Waiting_list` (
     `account_num` int unsigned NOT NULL,
     `class` varchar(50),
     PRIMARY KEY (`airline_id`, `flight_number`, `departure_date`, `account_num`),
-    FOREIGN KEY (`airline_id`, `flight_number`, `departure_date`) REFERENCES `Flight_instance` (`airline_id`, `flight_number`, `departure_date`),
-    FOREIGN KEY (`account_num`) REFERENCES `Customer` (`account_num`)
+    FOREIGN KEY (`airline_id`, `flight_number`, `departure_date`) REFERENCES `Flight_instance` (`airline_id`, `flight_number`, `departure_date`) on delete cascade on update cascade,
+    FOREIGN KEY (`account_num`) REFERENCES `Customer` (`account_num`) on delete cascade on update cascade
 );
 
-insert into waiting_list (airline_id, flight_number, departure_date, account_num, class) values ('AA', 1, '2021-12-01', 3, 'first_class');
+insert into waiting_list (airline_id, flight_number, departure_date, account_num, class) values ('AA', 1, '2021-12-01', 3, 'first class');
 
 CREATE TABLE IF NOT EXISTS `Includes` (
 	`airline_id` char(2) NOT NULL,
@@ -223,23 +215,13 @@ CREATE TABLE IF NOT EXISTS `Includes` (
     `departure_date` date,
     `ticket_number` int unsigned NOT NULL,
     `seat_number` int unsigned,
-    FOREIGN KEY (`airline_id`, `flight_number`, `departure_date`) REFERENCES `Flight_instance` (`airline_id`, `flight_number`, `departure_date`),
-    FOREIGN KEY (`ticket_number`) REFERENCES `Reserve_ticket` (`ticket_number`) on delete cascade
+    FOREIGN KEY (`airline_id`, `flight_number`, `departure_date`) REFERENCES `Flight_instance` (`airline_id`, `flight_number`, `departure_date`) on delete cascade on update cascade,
+    FOREIGN KEY (`ticket_number`) REFERENCES `Reserve_ticket` (`ticket_number`) on delete cascade on update cascade
 );
 
 insert into Includes (airline_id, flight_number, ticket_number, departure_date, seat_number) values ('AA', 1, 1, '2021-12-1', 1);
 insert into Includes (airline_id, flight_number, ticket_number, departure_date, seat_number) values ('UA', 3, 1, '2021-12-1', 3);
 
-CREATE TABLE IF NOT EXISTS `One_way` (
-	`ticket_number` int unsigned NOT NULL,
-    PRIMARY KEY (`ticket_number`),
-	FOREIGN KEY (`ticket_number`) REFERENCES `Reserve_ticket` (`ticket_number`)
-);
-CREATE TABLE IF NOT EXISTS `Round_trip` (
-	`ticket_number` int unsigned NOT NULL,
-    PRIMARY KEY (`ticket_number`),
-	FOREIGN KEY (`ticket_number`) REFERENCES `Reserve_ticket` (`ticket_number`)
-);
 CREATE TABLE IF NOT EXISTS `Question` (
 	`question_text` varchar(255),
     PRIMARY KEY (`question_text`)
@@ -248,11 +230,10 @@ CREATE TABLE IF NOT EXISTS `Answer` (
 	`question_text` varchar(255) NOT NULL,
     `answer_text` varchar(255) NOT NULL,
     PRIMARY KEY (`question_text`, `answer_text`),
-    FOREIGN KEY (`question_text`) REFERENCES `Question`(`question_text`)
+    FOREIGN KEY (`question_text`) REFERENCES `Question`(`question_text`) on delete cascade on update cascade
 );
 insert into Question(question_text) values ("How do I book a flight?");
 insert into Answer(question_text, answer_text) values ("How do I book a flight?", "Go to the Flights tab");
 insert into Question(question_text) values ("How much is a ticket?");
 insert into Answer(question_text, answer_text) values ("How much is a ticket?", "Depends on the flight but ranges from $100-$1000");
 insert into Question(question_text) values ('Is there a plane to my location?');
-
